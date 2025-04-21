5924812518f0cf1b927e7c178b01a06e94f0ff9c
unknown <ahsanakmalbit99@gmail.com>
2025-04-21 22:24:29 +0500

    Company Infomation

--- backend/server/models/company.js
+++ backend/server/models/company.js
@@ -1,50 +1,53 @@
 // models/passenger.js
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { ObjectId } = mongoose.Schema.Types;
 
-const companySchema = new mongoose.Schema({
+const companySchema = new mongoose.Schema(
+  {
     entrepreneurId: {
-        type: ObjectId,
-        ref: 'Entrepreneur',
-        required: [true, 'Entrepreneur is required'],
+      type: ObjectId,
+      ref: "Entrepreneur",
+      required: [true, "Entrepreneur is required"],
     },
     pitchTitle: {
-        type: String,
-        required: [true, 'Pitch title is required'],
-        unique: true,
+      type: String,
+      required: [true, "Pitch title is required"],
+      unique: true,
     },
     shortSummary: {
-        type: String,
-        required: [true, 'Short summary is required'],
+      type: String,
+      required: [true, "Short summary is required"],
     },
     website: {
-        type: String,
-        default: null,
+      type: String,
+      default: null,
     },
     companyBased: {
-        type: String,
-        required: [true, 'Where company based is required'],
+      type: String,
+      required: [true, "Where company based is required"],
     },
     industry: {
-        type: [String],
-        required: [true, 'Industry is required'],
+      type: [String],
+      required: [true, "Industry is required"],
     },
     stage: {
-        type: String,
-        required: [true, 'Stage is required']
+      type: String,
+      required: [true, "Stage is required"],
     },
     ideaInvestorRole: {
-        type: String,
-        required: [true, 'Idea investor role is required']
+      type: String,
+      required: [true, "Idea investor role is required"],
     },
     investmentRange: {
-        type: String,
-        required: [true, "Investment range is required"]
+      type: String,
+      required: [false, "Investment range is required"],
     },
     previousRoundRaise: {
-        type: String,
-        required: [true, 'Previous round raise is required']
-    }
-}, { timestamps: true });
+      type: String,
+      required: [true, "Previous round raise is required"],
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Company', companySchema);
+module.exports = mongoose.model("Company", companySchema);
